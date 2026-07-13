e8a0753ab6b34237f98497e5b0cd1e68663110a1
Len Liveasna_0010406 <liveasna10406@acledaaib.onmicrosoft.com>
2026-07-13 21:57:06 +0700

    connect database

---
+++ ProjectAssignment/ConDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace ProjectAssignment
+{
+    internal class ConDB
+    {
+        public static SqlConnection con = new SqlConnection();
+        public static void myConnection()
+        {
+            string conStr;
+            conStr = "Data Source = DESKTOP-17EIQD8\\SNADATABASE;" + "Initial Catalog=Inventory;" + "Integrated Security=true;";
+            try
+            {
+                con = new SqlConnection(conStr);
+                con.Open();
+                MessageBox.Show("Connection Successful");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+    }
+}
